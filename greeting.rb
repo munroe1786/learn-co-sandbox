76da25b2ec9e1ef4dc9e971142c51fdc9b6224c0
Eliza Munroe <era3munroe@gmail.com>
2021-06-25 13:47:31 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,26 @@
+enf#def greeting
+  #puts "Hello World"
+#end
+
+#def say_greeting_five_times
+  #greeting
+  #greeting
+  #greeting
+  #greeting
+  #greeting
+#end
+
+#say_greeting_five_times
+
+#def greeting_a_person(name)
+  #puts"Hello #{name}"
+#end
+
+#name = "Maria"
+#greeting_a_person(name)
+
+def greeting_programmer(name,language)
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
+
+greeting_programmer("Eliza","Ruby")
---
+++ learning-comments.rb
@@ -0,0 +1,77 @@
+# run_code_inside = false
+#puts "Code before if...end"
+#if run_code_inside
+  #puts "code inside"
+#end 
+#puts "Code after if...end"
+
+#chance_of_rain = 1.0
+#puts "Let's go outside!"
+#if chance_of_rain > 0.5
+  #puts "Pack an umbrella!"
+#else
+  #puts "Enjoy the fine day!"
+#end
+#puts "Oh, and always wear sunscreen!"
+#chance_of_rain = 0.74
+#if chance_of_rain <= 0.25
+  #puts "Pack a sun shelter!"
+#elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+  #puts "Pack an umbrella"
+#else
+  #puts "Stay home and read Hegel."
+#end
+#puts "You know what year it is??"
+#Time.now.year
+#puts "Hey, it's 2019!" if Time.now.year
+greeting = "unfriendly_greeting"
+
+#case greeting
+ # when "unfriendly_greeting"
+  #  puts "What do you want!?"
+  #whenhen "friendly_greeting"
+    #puts "Hi! How are you?"
+#end
+current_weather = "raining"
+
+#case current_weather
+ # when "sunny"
+    #puts "grab some sunscreen!"
+  #when "raining"
+    #puts "grab an umbrella"
+  #when "snowing"
+    #puts "bundle up"
+#end
+
+#n = 2
+#count = 0
+#while count <= n do
+  #puts "I ran."
+  #count += 1
+#end
+
+#3.times do
+  #puts "I ran."
+#end
+
+#count = 0
+#n = 3
+#loop do
+  #break if count >= n
+  #puts "I ran."
+  #count += 1
+#end
+
+#count = 0
+#n = 3
+#while true do
+  #break if count >= n
+  #puts "I ran."
+  #count += 1
+#end
+
+#counter = 0
+#until counter == 20
+  #puts "The current number is less than 20."
+  #counter += 1
+#end
